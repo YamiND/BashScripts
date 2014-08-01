daa3087e7b6ae1edd48b09a3d933f4a90c402c15
YamiND <comptvids@gmail.com>
2014-08-01 17:24:45 -0400

    Some stuff I install after a fresh install, hopefully this script works because I didn't test it

---
+++ UbuntuPostInstall.sh
@@ -0,0 +1,20 @@
+echo "Yami's typical post-installation stuff"
+
+gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
+
+sudo apt-get install vlc gimp libdvdread4 ubuntu-restricted-extras eclipse xchat
+sudo /usr/share/doc/libdvdread4/install-css.sh
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw
+sudo tlp start
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*; sudo apt-get -f install
+
+sudo apt-get update
+sudo apt-get dist-upgrade
