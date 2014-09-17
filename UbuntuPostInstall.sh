584b909a4db6f416f245b702c6c5e8c96eed1787
YamiND <comptvids@gmail.com>
2014-09-17 19:07:44 -0400

    updated for ssh and others

--- UbuntuPostInstall.sh
+++ UbuntuPostInstall.sh
@@ -2,7 +2,7 @@ echo "Yami's typical post-installation stuff"
 
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
-sudo apt-get install vlc gimp libdvdread4 ubuntu-restricted-extras eclipse xchat
+sudo apt-get install vlc gimp libdvdread4 ubuntu-restricted-extras eclipse xchat socat netbeans
 sudo /usr/share/doc/libdvdread4/install-css.sh
 
 sudo add-apt-repository ppa:webupd8team/java
@@ -15,6 +15,12 @@ sudo tlp start
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*; sudo apt-get -f install
+
+sudo apt-get install synaptic
+sudo apt-get install ssh
+sudo apt-get install android-tools-fastboot
+sudo apt-get install android-tools-adb
+
 
 sudo apt-get update
 sudo apt-get dist-upgrade
