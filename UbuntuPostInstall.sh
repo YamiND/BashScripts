274d9cee4d463245818001d6e1632044474c7c61
YamiND <comptvids@gmail.com>
2014-09-17 21:12:52 -0400

    hopefully automated install 100%

--- UbuntuPostInstall.sh
+++ UbuntuPostInstall.sh
@@ -2,25 +2,22 @@ echo "Yami's typical post-installation stuff"
 
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
-sudo apt-get install vlc gimp libdvdread4 ubuntu-restricted-extras eclipse xchat socat netbeans
-sudo /usr/share/doc/libdvdread4/install-css.sh
+sudo apt-get -y install vlc gimp libdvdread4 ubuntu-restricted-extras eclipse xchat socat netbeans android-tools-adb android-tools-fastboot ssh synaptic
+#sudo /usr/share/doc/libdvdread4/install-css.sh testing not having to use this part of the script
 
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
-sudo add-apt-repository ppa:linrunner/tlp
-sudo apt-get update
-sudo apt-get install tlp tlp-rdw
-sudo tlp start
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -y install oracle-java7-installer
+
+sudo add-apt-repository -y ppa:linrunner/tlp #not required for desktop
+sudo apt-get update #again not required for desktop
+sudo apt-get install tlp tlp-rdw #desktop computers don't need this
+sudo tlp start #again desktop computers don't need this
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*; sudo apt-get -f install
-
-sudo apt-get install synaptic
-sudo apt-get install ssh
-sudo apt-get install android-tools-fastboot
-sudo apt-get install android-tools-adb
+sudo dpkg -i google-chrome*; sudo apt-get -f -y install
 
 
-sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get update 
+sudo apt-get -y dist-upgrade 
