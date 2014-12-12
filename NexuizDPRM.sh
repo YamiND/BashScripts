84a1004b924d642846f9844c4c8d2bd7e51b9353
YamiND <comptvids@gmail.com>
2014-12-12 00:24:48 -0500

    Fixed folder permissions for the DPRM engine

--- NexuizDPRM.sh
+++ NexuizDPRM.sh
@@ -1,7 +1,7 @@
 sudo apt-get install git;
-git clone https://github.com/nexAkari/DarkPlacesRM.git
-cd DarkPlacesRM
-sudo apt-get install nexuiz-data libsdl-dev libjpeg62-dev nexuiz-textures nexuiz-music build-essential
+git clone https://github.com/nexAkari/DarkPlacesRM.git;
+cd DarkPlacesRM;
+sudo apt-get install nexuiz-data libsdl-dev libjpeg62-dev nexuiz-textures nexuiz-music build-essential;
 make sdl-nexuiz
 sudo make install
 cp ~/.nexuiz/data/config.cfg ~/.nexuiz/data/config.cfg.backup
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Bash Scripts
 =====
 
-These are a few bash scripts I use everytime I install a new operating system. Hopefully I can make this more seamless and add a few script to autonomize my distro upgrades.
+These are a few bash scripts I have written, or partially written that I find to be useful. Hopefully I can make this more seamless and add a few script to automate simple tasks.
+
