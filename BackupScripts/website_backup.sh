59cc16d7e18ffeb2b929364518c41daa8f003e69
Tyler Postma <comptvids@gmail.com>
2015-10-27 21:21:45 -0400

    Fixed up the backup of the directories

--- BackupScripts/website_backup.sh
+++ BackupScripts/website_backup.sh
@@ -3,9 +3,9 @@ MYSQL_PASS=""
 
 mysqldump -u root -p$MYSQL_PASS --all-databases > ~/backup/mysql/backup.sql
 
-cp /etc/apache2/sites-enabled/* backup/apache2/
-sudo cp -R /var/www/* backup/www/
+cp /etc/apache2/sites-available/* ~/backup/httpd/sites-available/
+sudo cp -R /var/www/* ~/backup/www/html/
 
 tar -zcvf backup.tar.gz -C  ~/backup .
 
-scp -r backup.tar.gz lakertech@35.58.0.104:/home/lakertech
+scp -r backup.tar.gz lakertech@35.58.0.108:/home/lakertech
