4f9f617ddb0bc0490cd9170120ec05e5704c926f
Tyler Postma <comptvids@gmail.com>
2015-10-29 22:55:07 -0400

    some additional information if you have a bad md5sum

--- CentOS/7/comparemd5.sh
+++ CentOS/7/comparemd5.sh
@@ -2,6 +2,10 @@
 
 inputfile=binaryver.csv
 badmd5file=badmd5.txt
+matchmd5file=matchmd5.txt
+searchfile=version.txt
+
+rm $badmd5file $matchmd5file $searchfile
 
 while read line; do
 
@@ -10,8 +14,14 @@ while read line; do
 	imagemd5=$(echo "$line" | cut -d ',' -f2)
 
 	if [ "$currmd5" == "$imagemd5" ]; then
-		echo "$binaryprog matches file md5"
+		echo "$binaryprog matches md5" >> $matchmd5file
 	else
 		echo "$binaryprog does not match" >> $badmd5file
+		# Yes this script assumes ls and awk are not screwed with
+		# Then again, if you have a miss matching md5 you have reason to worry
+		# So if you want reliability, change the script to use an external drive's
+		# version of the programs
+		badinfo=$(ls -al $binaryprog | awk '// {print $5 "," $6 "," $7}')
+		echo "$binaryprog info: $badinfo" >> $badmd5file
 	fi
  done < $inputfile
