6325874f8b2bf181b08a25bea4b5d30512db627f
yami <comptvids@gmail.com>
2015-11-08 14:05:16 -0500

    Different dependencies for different versions

--- Installs/vradiant.sh
+++ Installs/vradiant.sh
@@ -3,12 +3,22 @@
 #script to make vradiant work
 #had to do some minor changes to make it actually compile
 
-sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev sqlite-dev zlib1g-dev;
+ubuntuVersion=$(lsb_release -r | cut -f 2)
 
-git clone git://git.vecxis.org/vradiant.git ~/vradiant/;
-cd ~/vradiant/;
-make;
+if [ "$ubuntuVersion" = "15.10" ]
+then
+	sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev libsqlite3-dev zlib1g-dev
+else
+	sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev sqlite-dev zlib1g-dev
 
-echo "The file to launch is called radiant.bin in ~/vradiant/install/";
+fi
+
+git clone git://git.vecxis.org/vradiant.git ~/vradiant/
+
+cd ~/vradiant/
+
+make
+
+echo "The file to launch is called radiant.x86 in ~/vradiant/install/"
 
 
