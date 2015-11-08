33fa8c94657245a88d64a42a3c66b24cb7393ee9
yami <comptvids@gmail.com>
2015-11-08 15:41:01 -0500

    Packages were not different between 14.04 and 15.10, so statement not needed

--- Installs/vradiant.sh
+++ Installs/vradiant.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 
-#script to make vradiant work
-#had to do some minor changes to make it actually compile
+#################################
+# Please Read:  	        #
+# This script has been tested   #
+# on the following platforms:   #
+#			        #
+# Ubuntu 14.04 		        #
+# Ubuntu 15.10		        #
+#			        #
+# TODO:				# 
+# Install vradiant to the 	#
+# system directory as an actual #
+# application			#
+#################################
 
-ubuntuVersion=$(lsb_release -r | cut -f 2)
-
-if [ "$ubuntuVersion" = "15.10" ]
-then
-	sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev libsqlite3-dev zlib1g-dev
-else
-	sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev sqlite-dev zlib1g-dev
-
-fi
+sudo apt-get install git subversion g++ libgtkglext1-dev libjpeg8-dev liblzma-dev libxml2-dev libsqlite3-dev zlib1g-dev
 
 git clone git://git.vecxis.org/vradiant.git ~/vradiant/
 
@@ -20,5 +23,3 @@ cd ~/vradiant/
 make
 
 echo "The file to launch is called radiant.x86 in ~/vradiant/install/"
-
-
