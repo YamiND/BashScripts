f2b91fbab9eb7fb003d35e4c3be2b0355a2b72eb
yami <comptvids@gmail.com>
2015-11-09 22:50:12 -0500

    minor changes

--- CentOS/7/Auditing/compareMD5.sh
+++ CentOS/7/Auditing/compareMD5.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-inputfile=binaryver.csv
-badmd5file=badmd5.csv
-matchmd5file=matchmd5.txt
-searchfile=version.txt
+inputFile=filesImage.csv
+badMD5File=badMD5.csv
+searchFile=version.txt
 
-rm $badmd5file $matchmd5file $searchfile
+if [ $badMD5File -o $searchFile ]
+then
+	rm $badMD5File $searchFile
+fi
 
 while read line; do
 
@@ -13,14 +15,12 @@ while read line; do
 	currmd5=$(md5sum $binaryprog | cut -d ' ' -f1)
 	imagemd5=$(echo "$line" | cut -d ',' -f2)
 
-	if [ "$currmd5" == "$imagemd5" ]; then
-		echo "$binaryprog matches md5" >> $matchmd5file
-	else
-		# Yes this script assumes ls and awk are not screwed with
-		# Then again, if you have a miss matching md5 you have reason to worry
-		# So if you want reliability, change the script to use an external drive's
-		# version of the programs
+	if [ ! "$currmd5" == "$imagemd5" ]
+	then
 		badinfo=$(ls -al $binaryprog | awk '// {print $5 "," $6 "," $7}')
 		echo "$binaryprog does not match, $currmd5, $badinfo" >> $badmd5file
+	else
+		echo "$binaryprog matches md5"
+	
 	fi
  done < $inputfile
