48e6a7042789cd923d6a2c22bec9cfca80ec4df2
yami <comptvids@gmail.com>
2015-11-09 22:50:28 -0500

    Minor information differences

--- CentOS/7/Auditing/generateServerInfo.sh
+++ CentOS/7/Auditing/generateServerInfo.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
-rm binaryver.csv
+#!/bin/bash
+
+systemImage=~/systemImage.csv
 files="/bin/* /sbin/* /usr/bin/* /usr/sbin/*"
-for i in $files; do
+directories="/bin /sbin /usr/bin /usr/sbin"
+
+echo "System Image Report \t Generated on $(date -u)" > $systemImage
+echo "" >> $systemImage
+echo "Size of the directories: " >> $systemImage
+du -hcs $directories | sed \$d >> $systemImage
+echo "" >> $systemImage
+
+for i in $files;
+do
 	md5=$(md5sum $i | cut -d ' ' -f1)
 	info=$(ls -al $i | awk '// {print $5 "," $6 "," $7}')
-	echo "$i,$md5,$info" >> binaryver.csv
+	echo "$i,$md5,$info" >> $systemImage
 done
