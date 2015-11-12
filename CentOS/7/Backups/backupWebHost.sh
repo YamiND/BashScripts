ecfc9384dd12936636edd2b93ea3c2004e209634
yami <comptvids@gmail.com>
2015-11-11 21:43:25 -0500

    backup script ready for testing

--- CentOS/7/Backups/backupWebHost.sh
+++ CentOS/7/Backups/backupWebHost.sh
@@ -7,6 +7,7 @@
 # directories in /var/www/html			   #
 # This will also backup the database		   #
 ####################################################
+
 mkdir ~/tempDir
 mkdir ~/tempDir/www
 mkdir ~/tempDir/mysql
@@ -16,7 +17,8 @@ tempDir=~/tempDir
 wwwDirBackup=$tempDir/www
 mysqlBackup=$tempDir/sqlBackup.sql
 httpdConfigBackup=$tempDir/httpd.conf
-timeStamp=$(date -u)
+timeStamp=$(date +"%Y-%m-%d")
+backupArchive=~/backup$timeStamp.tar.gz
 
 read -p "Do you want to send the backup file via SSH? [y/n] " sshTransfer
 read -p "What is the password to the root MySQL/MariaDB account? " dbPass
@@ -44,5 +46,44 @@ mysqldump -u root -p$dbPass --all-databases > $mysqlBackup
 # Throw backup into single archive #
 ####################################
 
-tar -zcvf 
+tar -zcvf $backupArchive -C $tempDir .
 
+#######################################
+# Back up the files over SSH if opted #
+#######################################
+
+if [ "$sshTransfer" = "y" ]
+then
+	read -p "IP for SSH? " sshIP
+	read -p "Username for SSH? " sshUser
+	read -p "Password for SSH? " sshPass
+	read -p "Directory to copy to? " sshDir
+	read -p "Are you connecting over port 22? [y/n]" sshPort
+	
+	sshAuth="$sshUser@$sshIP:$sshDir"
+	
+	case $sshPort in
+
+		y)	
+		scp -r $backupArchive $sshAuth
+		;;
+		n)
+		read -p "What is the port number you wish to connect on? " sshNum
+		scp -P $sshNum $sshAuth
+		;;
+	esac
+	
+	echo "Backup Complete, the archive file is on $sshIP"
+fi
+
+##############################
+# Remove temporary Directory #
+##############################
+
+sudo rm -rf $tempDir
+
+###################
+# Echo completion #
+###################
+
+echo "Backup complete, the archive is called $backupArchive"
