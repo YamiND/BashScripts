e0b45c3b43bd7a71546daf51335e6095d35879a1
root <root@powerhouse.lakertech.com>
2016-02-02 17:03:55 -0500

    created seperate script for setting up sftp

---
+++ CentOS/7/SFTP/createSFTP.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user or run with sudo" 2>&1
+  exit 1
+fi
+
+checkFTP()
+{
+        sshConfig=/etc/ssh/sshd_config
+        oldSFTP="/usr/libexec/openssh/sftp-server"
+        newSFTP="internal-sftp"
+
+        if grep -q "$oldSFTP" "$sshConfig"
+        then
+
+                sed -i "s|$oldSFTP|$newSFTP|g" "$sshConfig"
+
+        elif grep -q "$newSFTP" "$sshConfig"
+        then
+
+                echo "SFTP Line already in $sshConfig"
+
+        else
+
+                echo "Subsystem sftp internal-sftp" >> $sshConfig
+
+        fi
+
+        if grep -q "sftpusers" "$sshConfig"
+        then
+
+                echo "Entry already in $sshConfig"
+        else
+
+        cat <<< '
+Match Group sftpusers
+        ChrootDirectory /sftp/%u
+        ForceCommand internal-sftp
+        X11Forwarding no
+        AllowTcpForwarding no
+        ' >> $sshConfig
+        fi
+}
+
+        checkFTP
+	mkdir /sftp
+	groupadd sftpusers
+
