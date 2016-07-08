72271b5d58977ad2ed8d4ca782ebcfce1892dcc7
Tyler Postma <comptvids@gmail.com>
2016-07-07 21:16:16 -0400

    Whitespace apparently matters after and before a \ key, whyyyy

--- OpenVPN/setupOpenVPN.sh
+++ OpenVPN/setupOpenVPN.sh
@@ -200,11 +200,11 @@ elif [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian" ]; then
 # Set up stupid UFW networking #
 ################################
 
-sed -i -e '/# Don\x27t delete these required lines, otherwise there will be errors/i \ 
-\*nat \ 
-:POSTROUTING ACCEPT \[0:0\] \ 
--A POSTROUTING -s 10\.8\.0\.0\/8 -o '"$routingInterface"' -j MASQUERADE \ 
-COMMIT ' /etc/ufw/before.rules
+sed -i '/# Don\x27t delete these required lines, otherwise there will be errors/i\
+\*nat\
+:POSTROUTING ACCEPT \[0:0\]\
+-A POSTROUTING -s 10\.8\.0\.0/8 -o '"$routingInterface"' -j MASQUERADE\
+COMMIT' /etc/ufw/before.rules
 
 sed -i "s/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g" /etc/default/ufw
 
